61a9c97b29ebe5589b6a0563694c7eff0e70bd2d
prakash <prakashbaireddy@gmail.com>
2024-12-30 10:17:02 +0530

    new changes

--- 01-shell-script.sh
+++ 01-shell-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-PERSON1=$1
-PERSON2=$2
+echo "enter your username"
 
-echo "$PERSON1: hi $PERSON2"
-echo "$PERSON2: hi $PERSON1"
-echo "$PERSON1: hellow, where are you"
-echo "$PERSON2: in my native place only"
+read USERNAME
+
+echo "enter you password"
+
+read PASSWORD
