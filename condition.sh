844033a5552ab3d7b9c7c42e6b5a49d13da0be21
prakash <prakashbaireddy@gmail.com>
2024-12-30 19:10:48 +0530

    new changes

--- condition.sh
+++ condition.sh
@@ -3,11 +3,12 @@
 lss -l
 
 if [ $? -ne 0]; then
-    echo "entered command has not found: $?"
+    echo "entered command has not found"
+    exit 1
 fi
 
 ls -l
 
 if [ $? -e 0]; then
-    echo "please find the results above: $?"
+    echo "please find the results above"
 fi
