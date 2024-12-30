c5f59e2324d4bb2a3a973dce0540d6620a6e4fe1
prakash <prakashbaireddy@gmail.com>
2024-12-30 19:18:44 +0530

    new changes

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-dnf install nginx -y
-
-echo "installation success"
+USERID=$(id -u)
+dnf install git -y
+if [ $USERID -ne 0 ]; then
+    echo "you don't have access to execute this script, please contact sysadm"
+fi
