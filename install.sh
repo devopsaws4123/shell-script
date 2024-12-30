cf9b25141caa9a505c0699b19279e358d37345ab
prakash <prakashbaireddy@gmail.com>
2024-12-30 19:21:29 +0530

    new changes

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
-dnf install git -y
+dnf install httpd -y
 if [ $USERID -ne 0 ]; then
     echo "you don't have access to execute this script, please contact sysadm"
+    else
+    echo "httpd has installed successfully"
 fi
