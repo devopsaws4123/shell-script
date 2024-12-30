7071b9ba7750ee7bf74b6fd454b161d7d8181e1e
prakash <prakashbaireddy@gmail.com>
2024-12-30 10:02:21 +0530

    new changes

--- 01-shell-script.sh
+++ 01-shell-script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 
-echo "hello world"
+echo "hello world, please see below"
+
+echo "ramesh: hi suresh"
+echo "suresh: hi ramesh"
+echo "ramesh: hellow, where are you"
+echo "suresh: in my native place only"
