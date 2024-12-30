552856af28b5b62550302e105cc03a305cfbcb24
prakash <prakashbaireddy@gmail.com>
2024-12-30 21:44:09 +0530

    new changes

--- backend.sh
+++ backend.sh
@@ -7,7 +7,7 @@ LOG="backend-$DATE.log"
 RED="\e[31m"
 GREEN="\e[32m"
 NORMAL="\e[0m"
-
+LOG1="backend-${DATE}.log"
 if [ $USERID -ne 0 ]; then
     echo -e "$RED you don't have access to run this file $NORMAL"
     exit 1
