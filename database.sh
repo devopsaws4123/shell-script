6c87cb654f38576a954e98e68cb29c869f1e3294
prakash <prakashbaireddy@gmail.com>
2024-12-31 13:23:29 +0530

    new changes

--- database.sh
+++ database.sh
@@ -7,7 +7,8 @@ RED="\e[32m"
 GREEN="\e[31m"
 NORMAL="\e[0m"
 
-if [ $USERID -ne 0 ] then
+if [ $USERID -ne 0 ]
+then
     echo "Timestamp: $DATE"
     echo -e " $RED you don't have access to run this file $NORMAL"
 fi
