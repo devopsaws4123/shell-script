22e0b00cacc3cd070c8125318a29d52f0aa3ae24
prakash <prakashbaireddy@gmail.com>
2024-12-31 14:41:12 +0530

    new changes

--- file.sh
+++ file.sh
@@ -4,7 +4,10 @@ USERID=$(id -u)
 
 if [ $USERID -eq 1001 ]
 then
-touch user.txt
+touch user1.txt
+    echo "file has been created successfully"
 else
-    echo "user is not a root user"
+    echo "user is not a ec2-user user"
 fi
+
+vim user1.txt
