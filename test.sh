6e3dafd1b325f2553d4fdadb3a4bf468c40add63
prakash <prakashbaireddy@gmail.com>
2024-12-31 13:45:15 +0530

    new changes

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ PWD=$(pwd)
 
 echo "You are here: $PWD"
 
-touch testing.txt
+FILE=$(touch testing1.txt)
 if [ $? -ne 0 ]
 then
     echo "files has not been created"
 else
-    echo "File has been created successfully"
+    echo "File has been created successfully: $FILE"
 fi
