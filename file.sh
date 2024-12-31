4c718ad0b9e0a7dbb835ef3c9e3b03400bf08130
prakash <prakashbaireddy@gmail.com>
2024-12-31 19:33:36 +0530

    new changes

--- file.sh
+++ file.sh
@@ -5,5 +5,5 @@
 vim user2.txt <<EndOfCommands
 i
 This file was created automatically from a shell script
-^[ZZ
+^[ZZ 
 EndOfCommands
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 DATE=$(date)
 R="\e[31m"
 N="\e[0m"
-LOG="mysql-server-$DATE.log"
+
 
 if [ $USERID -ne 0 ]; then
     echo -e " $R You Don't have access to run this script $N"
@@ -13,21 +13,25 @@ fi
 
 FUNCTION(){
 if [ $1 -ne 0 ]; then
-    echo -e "$2...failed" 2>&1 | tee >>LOG
+    echo -e "$2...failed" 2>&1 | tee 
     exit 1
 else
-    echo -e "$2..is Success" 2>&1 | tee >>LOG
+    echo -e "$2..is Success" 2>&1 | tee 
 fi
 }
 
-dnf install mysql-server -y >>LOG
+dnf install mysql-server -y 
 
 FUNCTION $? "installed mysql server"
 
-systemctl enable mysqld >>LOG
+systemctl enable mysqld 
 
-FUNCTION $? "Enabled Mysqld" >>LOG
+FUNCTION $? "Enabled Mysqld" 
 
-systemctl start mysqld >>LOG
+systemctl start mysqld 
 
 FUNCTION $? "mysql service started"
+
+PROCESS=$(ps -ef | grep mysql)
+
+echo "$PROCESS"
