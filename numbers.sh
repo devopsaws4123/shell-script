5440c884168b887894fbd48fd5fd8097e31705e8
prakash <prakashbaireddy@gmail.com>
2025-01-02 21:11:33 +0530

    new changes

--- numbers.sh
+++ numbers.sh
@@ -4,13 +4,12 @@ DATE=$(date)
 echo "Todat date is: $DATE"
 echo "please find the results below:"
 
-for i in {1..100}
-do
-    if [ 'expr $i % 3' == 0 ]
-    then
-        echo $i
-    fi
-done
+
+
+if [ 'expr $i % 3' == 0 ]
+then
+    echo $i
+fi
 
 for package in 'cat textfile.txt'
 do 
