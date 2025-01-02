c469e7f634e720c3a04c8db0a345d037963963d5
prakash <prakashbaireddy@gmail.com>
2025-01-02 14:15:56 +0530

    new changes

--- for-loop.sh
+++ for-loop.sh
@@ -6,11 +6,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER_NAME="/var/log/shellscript"
+LOG_FOLDER_NAME="/var/log/shellscript-log"
 DATE=$(date +%Y-%m-%d)
 LOG_FILE=$(echo $0)
 LOG_FILE_NAME="$LOG_FOLDER_NAME/$LOG_FILE-$DATE.log"
 
+echo "script started at : $DATE"
 for i in {1..10}
 do 
     echo $i &>>$LOG_FILE_NAME
