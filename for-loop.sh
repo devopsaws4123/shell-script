086630a46a6140fb072c08934f9774b7b8abe25b
prakash <prakashbaireddy@gmail.com>
2025-01-02 14:20:59 +0530

    new changes

--- for-loop.sh
+++ for-loop.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOG_FOLDER_NAME="/var/log/shellscript-log"
 DATE=$(date +%Y-%m-%d)
-LOG_FILE=$(echo $0)
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 LOG_FILE_NAME="$LOG_FOLDER_NAME/$LOG_FILE-$DATE.log"
 
 echo "script started at : $DATE"
