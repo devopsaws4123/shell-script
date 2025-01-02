fae3fe844aaebb2782ac99f3bfed360f1caff4d0
prakash <prakashbaireddy@gmail.com>
2025-01-02 20:41:58 +0530

    new changes

--- numbers.sh
+++ numbers.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 DATE=$(date)
+echo "Todat date is: $DATE"
+echo "please find the results below:"
 
 for i in {1..10}
 do
-    echo "please find the results below:"
+    
     echo "$i"
 done
