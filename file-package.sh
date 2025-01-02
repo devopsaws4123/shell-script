2bfaf8390f7084c4050d29f85a9d1ec71d7e0abd
prakash <prakashbaireddy@gmail.com>
2025-01-02 20:08:08 +0530

    new changes

--- file-package.sh
+++ file-package.sh
@@ -10,23 +10,35 @@ Y="\e[33m"
 N="\e[0m"
 # codes are using for colurs -end
  
-# LOG_PATH="/home/ec2-user/shell-script/shellscript-logs"    # This is for path of the log file 
-# FILE=$(echo $0 | cut -d "." f1)                            # it will retrive the current executing file name
-# DATE=$(date +%Y-%m-%d-%H-%M-%S)                            #it will print the date time
-# OUTPUT_LOG="$LOGPATH/$FILE-$DATE.log"                      # Here we are concatinating and storing the log file 
+LOG_PATH="/home/ec2-user/shell-script/shellscript-logs"    # This is for path of the log file 
+FILE=$(echo $0 | cut -d "." f1)                            # it will retrive the current executing file name
+DATE=$(date +%Y-%m-%d-%H-%M-%S)                            #it will print the date time
+OUTPUT_LOG="$LOGPATH/$FILE-$DATE.log"                      # Here we are concatinating and storing the log file 
 
 if [ $USERID -ne 0 ]
 then
     echo -e " $R You Don't Have Access to Execute This File, Plese Check With Admin Team $N"
 fi
 
-for $package in 'cat /home/ec2-user/shell-script/packages.txt'
+echo "$0 has executed at : $date"
+
+PACKAGE(){
+if [ $1 -ne 0 ]
+then
+    echo "$2 installation has failed"
+    exit 1
+else
+    echo "$2 installing"
+}
+
+for $service in $@
 do
-    echo "$package is installing"
-    if [ $? -ne 0 ]
-    then
-        echo "$package $R installation has failed $N "
-    else
-        echo "$package $G has been installed successfully $N"
-    fi
+    dnf install $service -y &>>
+    $PACKAGE $? echo " $service $G Installed $N "
+
+    dnf install $service -y
+    $PACKAGE $? echo " $service $G installed $N "
+
+    dnf install $service -y
+    $PACKAGE $ echo " $service $G installed $N "
 done
